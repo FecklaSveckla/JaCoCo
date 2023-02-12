a79e7711869812ac596831faf62ecf545d3741f7
Beloglazova Olga <beloglazovaom@yandex.ru>
2023-02-12 17:46:23 +0300

    Finaly add

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax2() {
+        StatisticsService variable = new StatisticsService();
+
+        long[] incomesInEuro = {5, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long predicted = 12;
+
+        long actual = variable.findMax(incomesInEuro);
+
+        assertEquals(predicted,actual);
+    }
 }
